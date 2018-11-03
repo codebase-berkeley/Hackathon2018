fd0c79ead1eb8aab7f9d9f6c86ee0ab1e7e3869e
Kjue <kelvinjue@gmail.com>
2018-11-03 15:14:45 -0700

    i hope this works

---
+++ components/maps/Maps.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { MapView, Marker } from 'react-native-maps';
+
+export default class Maps extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+      </View>
+    );
+  }
+}
